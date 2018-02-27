54f2c814cc912ea09c0ff1c1153773a55f38dca9
Bryan Kroon <SuperGeneralCrazy@gmail.com>
2018-02-27 13:57:32 +0100

    Base Assembly

---
+++ QuizGames/QuizGames.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGames.Entities
+{
+    public class Class1
+    {
+    }
+}
