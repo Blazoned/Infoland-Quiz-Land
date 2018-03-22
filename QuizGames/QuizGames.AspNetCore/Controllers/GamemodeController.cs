3673376bfc3acee88674bd1de9fec246185876d3
leroy112 <leroylyon@hotmail.com>
2018-03-22 15:39:58 +0100

    vraag UI aangemaakt. viewmodel nog koppelen aan model en checken of bijwerken van het antwoord werkt.

---
+++ QuizGames/QuizGames.AspNetCore/Controllers/GamemodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuizGames.AspNetCore.ViewModels;
+
+namespace QuizGames__Asp.Net_Core_.Controllers
+{
+    public class GamemodeController : Controller
+    {
+        public IActionResult Camelrace(GameModeViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ QuizGames/QuizGames.AspNetCore/ViewModels/GameModeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizGames.AspNetCore.ViewModels
+{
+    public class GameModeViewModel
+    {
+        #region Properties
+
+        //all properties that are needed for the view and model to comunicate
+        public string Question = " is sebas de speknek?";
+        public string Anwser1 = "ja";
+        public string Anwser2 = "nee";
+        public string Anwser3 = "weet ik veel";
+        public string Anwser4 = "ik ben bart en stem VVD dus ik laat me niet uit over deze stelling aangezien ik altijd PC ben";
+        public string ChosenAnwser { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        public void SetChosenAnwser(string anwser)
+        {
+            ChosenAnwser = anwser;
+        }
+
+        #endregion
+
+    }
+}
