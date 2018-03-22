f48987781941fa89e1ea2f97435b69b33c452266
Blazoned <bryankroon@ziggo.nl>
2018-03-22 10:09:54 +0100

    TEMP

--- QuizGames/QuizGames.AspNetCore/Controllers/HomeController.cs
+++ QuizGames/QuizGames.AspNetCore/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuizGames.ViewModels;
 
 namespace QuizGames__Asp.Net_Core_.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+
+
+            return View(model);
         }
     }
 }
---
+++ QuizGames/QuizGames.ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGames.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name="Login")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Password")]
+        public string Pass { get; set; }
+    }
+}
