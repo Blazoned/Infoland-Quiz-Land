988f51f9776b34307db5eba133664d650ba35ccc
Willems,Sebastiaan T.S <369661@student.fontys.nl>
2018-03-29 13:05:22 +0100

    commented the menu stuff

--- QuizGames/QuizGames.AspNetCore/Controllers/MenuController.cs
+++ QuizGames/QuizGames.AspNetCore/Controllers/MenuController.cs
@@ -5,17 +5,17 @@ namespace QuizGames__Asp.Net_Core_.Controllers
 {
     public class MenuController : Controller
     { 
-        [HttpGet]
-        public IActionResult Menu()
-        {
-            return View();
-        }
+        //[HttpGet]
+        //public IActionResult Menu()
+        //{
+        //    return View();
+        //}
 
-        [HttpPost]
-        public IActionResult Menu()
-        {
+        //[HttpPost]
+        //public IActionResult Menu()
+        //{
             
-        }
+        //}
 
     }
 }
