57b62179e186fd1505174e41dfaa353228ac451a
Willems,Sebastiaan T.S <369661@student.fontys.nl>
2018-04-10 10:00:57 +0200

    camelrace

---
+++ QuizGames/QuizGames.AspNetCore/wwwroot/js/race.js
@@ -0,0 +1,25 @@
+var elem = id1;
+
+function move(id1, id2) {
+  var elem = id1;
+  var img = id2;
+  var width2 = 1;
+  var width = Number(elem.style.width.replace(/[^\d\.\-]/g, ''));
+  var name = elem.name;
+  if (width < 90){
+  width+= 10; 
+  elem.style.width = width + '%'; 
+  img.style.left = width  + '%';
+  }
+  else {
+  alert("Game over!");
+  Camelbar1.style.width = 5 + '%';
+  Camelbar2.style.width = 5 + '%';
+  Camelbar3.style.width = 5 + '%';
+  Camelbar4.style.width = 5 + '%';
+  img1.style.left = 5 + '%';
+  img2.style.left = 5 + '%';
+  img3.style.left = 5 + '%';
+  img4.style.left = 5 + '%';
+  }
+}
