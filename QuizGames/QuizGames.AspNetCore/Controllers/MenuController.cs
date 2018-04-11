871518d44eef91abf555b012011b53213988f046
Bart Rijnders <bart_rijnders@hotmail.com>
2018-04-11 12:46:34 +0200

    bart heeft mvc geleerd bedankt voor het geduld!

--- QuizGames/QuizGames.AspNetCore/Controllers/MenuController.cs
+++ QuizGames/QuizGames.AspNetCore/Controllers/MenuController.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuizGames.ViewModels;
 
+
 namespace QuizGames__Asp.Net_Core_.Controllers
 {
     public class MenuController : Controller
-    { 
-        //[HttpGet]
-        //public IActionResult Menu()
-        //{
-        //    return View();
-        //}
+    {
+        [HttpGet]
+        public ActionResult Menu()
+        {
+            return View();
+        }
 
         //[HttpPost]
-        //public IActionResult Menu()
+        //public IActionResult play()
         //{
-            
+        //    return RedirectToAction();
         //}
 
     }
