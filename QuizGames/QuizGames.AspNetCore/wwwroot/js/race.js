dc1bf9732d861d1c8a41d674eb17ed67bbfbc270
Bart Rijnders <bart_rijnders@hotmail.com>
2018-05-31 14:07:42 +0200

    the true one (alleen dingen toevoegen seb!)

--- QuizGames/QuizGames.AspNetCore/wwwroot/js/race.js
+++ QuizGames/QuizGames.AspNetCore/wwwroot/js/race.js
@@ -30,7 +30,7 @@ function questionAsnsered(awnser) {
             nextQuestion(currentquestion);
             $(".answer").show();
         }, 1000);
-        
+
     }
     else {
         $(".answer").hide();
@@ -44,7 +44,6 @@ function questionAsnsered(awnser) {
             nextQuestion(currentquestion);
             $(".answer").show();
         }, 1000);
-
     }
 }
 
@@ -58,83 +57,45 @@ function updatebars(qQuestions) {
 
     var positions = [positionP1, positionP2, positionP3, positionP4];
 
-    if (player1points == qQ) {
-        // Get the modal
-        var modal = document.getElementById('myModal');
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+    if ($(window).width() > 1200) {
 
-        // When the user clicks the button, open the modal
-        btn.onclick = function () {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-        gameOver();
+        positions.forEach(function (value, entry) {
+            var i = entry + 1;
+            document.getElementById("Camelbar" + i).style.width = positions[entry] + "%";
+            document.getElementById("Camelbar" + i).style.height = "50px";
+            document.getElementById("img" + i).style.display = "block";
+            document.getElementById('crp' + 1).innerHTML = points[entry];
+        });
     }
-    else if (player2points == qQ || player3points == qQ || player4points == qQ) {
-        alert("You Lost :(")
-        gameOver();
-    }
-        else {
-        if ($(window).width() > 1200) {
-            document.getElementById("Camelbar1").style.width = positionP1 + '%';
-            document.getElementById("Camelbar2").style.width = positionP2 + '%';
-            document.getElementById("Camelbar3").style.width = positionP3 + '%';
-            document.getElementById("Camelbar4").style.width = positionP4 + '%';
+    else {
 
-            document.getElementById("Camelbar1").style.height = "50px";
-            document.getElementById("Camelbar2").style.height = "50px";
-            document.getElementById("Camelbar3").style.height = "50px";
-            document.getElementById("Camelbar4").style.height = "50px";
+        positions.forEach(function (value, entry) {
+            var i = entry + 1;
+            document.getElementById("Camelbar" + i).style.height = positions[entry] + "%";
+            document.getElementById("Camelbar" + i).style.width = "100%";
+            document.getElementById("img" + i).style.display = "none";
+            document.getElementById('crp' + i).innerHTML = points[entry];
 
-            document.getElementById("img1").style.display = "block";
-            document.getElementById("img2").style.display = "block";
-            document.getElementById("img3").style.display = "block";
-            document.getElementById("img4").style.display = "block";
-        }
-        else {
-
-            positions.forEach(function (value, entry) {
-                var i = entry + 1;
-                document.getElementById("Camelbar" + i).style.height = positions[entry] + "%";
-                document.getElementById("Camelbar" + i).style.width = "100%";
-                document.getElementById("img" + i).style.display = "none";
-                document.getElementById('crp' + i).innerHTML = points[entry];
-                
-            });
+        });
     }
 
     if (points[0] == qQ) {
-        
+        alert("YOU WON!")
         if (finish == false) {
             gameOver();
         }
-        alert("YOU WON!")
-        
+
+
     }
     else if (points[1] == qQ || points[2] == qQ || points[3] == qQ) {
-        
+        alert("you lose")
         if (finish == false) {
             gameOver();
         }
-        alert("you lose")
     }
-    
+
 }
 
 function gameOver() {
@@ -152,7 +113,7 @@ function gameOver() {
 
 function nextQuestion(lastquestion) {
     currentquestion = Math.floor((Math.random() * 5) + 0);
-    
+
     var x = document.getElementsByClassName('cquestion');
     var i;
     for (i = 0; i < x.length; i++) {
@@ -167,7 +128,7 @@ function nextQuestion(lastquestion) {
     document.getElementById('canswer1').innerHTML = questions[currentquestion].answers[0];
     document.getElementById('canswer2').innerHTML = questions[currentquestion].answers[1];
     document.getElementById('canswer3').innerHTML = questions[currentquestion].answers[2];
-    document.getElementById('canswer4').innerHTML = questions[currentquestion].answers[3]; 
+    document.getElementById('canswer4').innerHTML = questions[currentquestion].answers[3];
 }
 
 function toggleGameplay() {
@@ -183,7 +144,7 @@ function toggleGameplay() {
     }
 }
 
-function DummyQuestions(){
+function DummyQuestions() {
     var q =
         [
             {
@@ -219,13 +180,6 @@ function DummyQuestions(){
     return q;
 }
 
-function updateScore() {
-    document.getElementById('crp1').innerHTML = player1points;
-    document.getElementById('crp2').innerHTML = player2points;
-    document.getElementById('crp3').innerHTML = player3points;
-    document.getElementById('crp4').innerHTML = player4points;
-}
-
 function random_points() {
     var i;
     for (i = 0; i < 3; i++) {
@@ -234,20 +188,18 @@ function random_points() {
             if (i == 0) {
                 points[1]++;
                 updatebars(qQ);
-                
+
             }
             else if (i == 1) {
                 points[2]++;
                 updatebars(qQ);
-                
+
             }
             else if (i == 2) {
                 points[3]++;
                 updatebars(qQ);
-                
+
             }
         }
-    }  
+    }
 }
-
-
