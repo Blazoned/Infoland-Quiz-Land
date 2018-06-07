703830850352dc2d503203c35ba2c2c12e1ee67f
Blazoned <bryankroon@ziggo.nl>
2018-06-07 11:22:52 +0200

    WIP: Score keeping of a user. TODO: players is null -> player already signed in?

---
+++ QuizGames/QuizGames.AspNetCore/Hubs/GameHub.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using QuizGames.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizGames.AspNetCore.Hubs
+{
+    public class GameHub : Hub
+    {
+        // Contains a global count of players (game management)
+        public static List<GameViewModel> players;
+
+        // A client attempts to join the game
+        public async Task JoinGame(string playerId)
+        {
+            if (players == null || players.Count < 4)
+            {
+                // Check if the player is already in game
+                if (players != null || players.Count((item) => { return item.PlayerId == playerId; }) >= 1)
+                {
+                    await Clients.Caller.SendAsync("GameConnected", -2);
+                    return;
+                }
+
+                // Create player
+                GameViewModel player = new GameViewModel(Context.ConnectionId, playerId);
+
+                // Retrieve currently connected players
+                await Clients.Caller.SendAsync("GameConnected", players);
+
+                // Create a player list (if null) add the new connection
+                players = players ?? new List<GameViewModel>();
+                players.Add(player);
+
+                // Send the connection to the other players and confirm connection
+                await Clients.Others.SendAsync("PlayerConnected", player);
+            }
+            else
+            {
+                // Affirm connection
+                await Clients.Caller.SendAsync("GameConnected", -1);
+            }
+        }
+
+        // A client attempts to leave the game
+        public async Task LeaveGame()
+        {
+            if (players.Count(item => { return item.ConnectionId == Context.ConnectionId; }) >= 1)
+            {
+                // Get player
+                GameViewModel player = players.First(item => { return item.ConnectionId == Context.ConnectionId; });
+
+                // Remove the player from the list
+                players.Remove(player);
+
+                // Reset the player score
+                await Clients.Others.SendAsync("ReceiveScore", player, 0);
+
+                // Send disconnection to other players and confirm disconnection
+                await Clients.Others.SendAsync("PlayerDisconnected", player);
+                await Clients.Caller.SendAsync("GameDisconnected", true);
+            }
+            else
+            {
+                // Affirm disconnection (can't disconnect when not connected)
+                await Clients.Caller.SendAsync("GameDisconnected", false);
+            }
+        }
+
+        // A player attempts to update their (online) score
+        public async Task SendScore(int score)
+        {
+            // Send score to other clients
+            await Clients.Others.SendAsync("ReceiveScore", players.First((item) => { return item.ConnectionId == Context.ConnectionId; }), score);
+        }
+    }
+}
---
+++ QuizGames/QuizGames.AspNetCore/wwwroot/js/gameHub.js
@@ -0,0 +1,94 @@
+﻿//
+// Variables
+
+// Contains the connected players & your player name
+var playerName; // TODO: Assign the username of a player to this variable
+var players = new Array(); // TODO: Use player names to keep track of the user
+
+
+//
+// Create connection to game hub
+let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub/game")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+//
+// Window close event (prevents players from taking up player space when they left the game)
+window.onbeforeunload = function () {
+    connection.invoke("LeaveGame");
+};
+
+
+//
+// Add connection handlers
+
+// Client attempted to connect to the game
+connection.on("GameConnected", function (playerClients) {
+    if (playerClients === -1) {
+        // Alert and redirect to previous page
+        alert("The game is already full!");
+        history.go(-1);
+    }
+    else if (playerClients === -2)
+    {
+        // Alert and redirect to previous page
+        alert("You're already in the game!");
+        history.go(-1);
+    }
+    else {
+        // Add previously connected clients to player list
+        for (let i = 0; i < playerClients.length; i++) {
+            players[i] = playerClients[i];
+        }
+
+        // Notify console
+        console.log("Connected!");
+    }
+});
+
+// A player connected to the game
+connection.on("PlayerConnected", function (player) {
+    // Add player to the player list
+    players[players.length] = player;
+
+    // Get personal score
+    let index = players.findIndex(function (item) {
+        return item.playerId === playerId;
+    });
+
+    // Send the clients current score to the newly connected player
+    connection.invoke("SendScore", players[index].score);
+});
+
+// A player disconnected from the game
+connection.on("PlayerDisconnected", function (player) {
+    // Find player in the player list
+    let index = players.findIndex(function (item) {
+        return item.playerId === player.playerId;
+    });
+
+    // Remove the player from the list
+    players.splice(index, 1);
+});
+
+// Change a player's score
+connection.on("ReceiveScore", function (player, score) {
+    // Find player in the player list
+    let index = players.findIndex(function (item) {
+        return item.playerId === player.playerId;
+    });
+
+    // Assign new score
+    players[index].score = score;
+});
+
+//
+// Start connection and attempt to join the game
+connection.start()
+    .then(function () {
+        connection.invoke("JoinGame", "User Alpha");
+    })
+    .catch(function (error) {
+        console.error(error.toString());
+    });
